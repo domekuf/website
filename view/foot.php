b73de4b2930b6553ebc019a715b737a60c4cf1cb
Domenico Di Iorio <dome.diiorio@gmail.com>
2019-01-14 23:35:11 +0100

    Fixed modal close button

--- view/foot.php
+++ view/foot.php
@@ -1,18 +1,19 @@
-        <div id="sample-modal" class="modal fade" role="dialog">
+        <div id="sample-modal" class="modal fade" role="dialog" tabindex="-1">
             <div class="modal-dialog">
                 <!-- Modal content-->
                 <div class="modal-content" style="background-color: black;border-color:grey">
-                  <div class="modal-header">
-                    <h4 class="modal-title">Details</h4>
-                </div>
-                <div class="modal-body">
-                    <p>
-                    Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In dignissim laoreet velit vel aliquet. Maecenas lacinia erat vitae aliquam pellentesque. Donec at magna volutpat, ultricies sem vitae, mollis risus. Morbi porttitor pellentesque ultricies. Fusce turpis quam, volutpat auctor hendrerit id, vulputate sed ligula. Nam in hendrerit felis, ac dapibus enim. Fusce rutrum turpis at suscipit efficitur. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nullam id urna ut lectus volutpat aliquam. Donec auctor, sapien sagittis molestie venenatis, dui sapien sagittis neque, pellentesque accumsan sapien lorem vel lacus. Duis metus dui, congue a ante at, tincidunt lobortis leo.
-                    </p>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                </div>
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal"><span style="color:white" aria-hidden="true">&times;</span></button>
+                        <h4 class="modal-title">Details</h4>
+                    </div>
+                    <div class="modal-body">
+                        <p>
+                        Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In dignissim laoreet velit vel aliquet. Maecenas lacinia erat vitae aliquam pellentesque. Donec at magna volutpat, ultricies sem vitae, mollis risus. Morbi porttitor pellentesque ultricies. Fusce turpis quam, volutpat auctor hendrerit id, vulputate sed ligula. Nam in hendrerit felis, ac dapibus enim. Fusce rutrum turpis at suscipit efficitur. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nullam id urna ut lectus volutpat aliquam. Donec auctor, sapien sagittis molestie venenatis, dui sapien sagittis neque, pellentesque accumsan sapien lorem vel lacus. Duis metus dui, congue a ante at, tincidunt lobortis leo.
+                        </p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    </div>
                 </div>
             </div>
         </div>
