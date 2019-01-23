fc20c7f036c2abf6550344b677be7df1e6d6e02d
Domenico Di Iorio <dome.diiorio@gmail.com>
2019-01-23 21:51:20 +0100

    Updating img in progress

--- view/contacts.php
+++ view/contacts.php
@@ -4,15 +4,7 @@
         <div class="col-lg-8 col-lg-offset-2">
             <h2><?=$T->trans($title)?></h2>
             <p>
-              <a href="mailto:e.brandolini@skyron.it">e.brandolini@skyron.it</a>
-            <br/>
-              <a href="mailto:a.previdi@skyron.it">a.previdi@skyron.it</a>
-            <br/>
-              <a href="mailto:o.dubaldo@skyron.it">o.dubaldo@skyron.it</a>
-            <br/>
-              <a href="mailto:n.gerin@skyron.it">n.gerin@skyron.it</a>
-            <br/>
-              <a href="mailto:ufficiotecnico@skyron.it">ufficiotecnico@skyron.it</a>
+              <a href="mailto:info@skyron.it">info@skyron.it</a>
             </p>
             <ul class="list-inline banner-social-buttons">
                 <li>
--- view/home.php
+++ view/home.php
@@ -11,7 +11,7 @@
                     <p>
 Skyron is a studio that provides talents in yacht design and engineering, bonded in a structured, homogeneous and organized work group.
 Skyron strongly believes in specific individual skills, giving value to personal ability and creativity.
-Born in 2013 by the intiative of a group of engineers and designers, the company has enstablished throughout the years a strong partnership with well known companies on both national and international land, and a network of suppliers.
+Born in 2013 by the intiative of a group of engineers and designers, the company has enstablished throughout the years a strong partnership with well known companies on both national and international land, and a wide network of suppliers.
                     </p>
                     <a href="#services" class="btn btn-circle page-scroll">
                         <i class="fa fa-angle-double-down animated"></i>
